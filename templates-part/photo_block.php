2a7bc35181090dc5676d5fdd270ec1f08e2373a7
Amelie-pixel <148459753+Amelie-pixel@users.noreply.github.com>
2024-05-13 13:28:39 +0200

    ajustement block photo

--- templates-part/photo_block.php
+++ templates-part/photo_block.php
@@ -1,4 +1,4 @@
-<div class="photo-block__picture">
+<div class="photo-block__picture flexrow">
     <div class="photo-block__picture__img_container">
         <img src="<?php the_post_thumbnail_url(); ?>" class="photo-block__picture__img">
         <div class="photo-block__picture__img_hover">
@@ -6,7 +6,9 @@
             <a href="<?php echo get_permalink(get_the_ID()); ?>">
                 <img class="oeil" src="<?php echo get_stylesheet_directory_uri() ;?>/assets/eye.svg" alt="Oeil">
             </a>
-            <img id="fullscreen" class="fullscreen open-lightbox" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/fullscreen.svg" alt="pleins écran" onclick="openLightbox('<?php the_post_thumbnail_url(); ?>', '<?php echo get_field('reference'); ?>', '<?php echo implode(', ', wp_list_pluck(get_the_terms(get_the_ID(), 'categorie'), 'name')); ?>')">
+            <div class="fullscreen">
+            <img id="fullscreen" class=" open-lightbox" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/fullscreen.svg" alt="pleins écran" onclick="openLightbox('<?php the_post_thumbnail_url(); ?>', '<?php echo get_field('reference'); ?>', '<?php echo implode(', ', wp_list_pluck(get_the_terms(get_the_ID(), 'categorie'), 'name')); ?>')">
+            </div>
             </div>
             <div class="ref_cat flexrow">
                 <p class="photo_reference_hover">
